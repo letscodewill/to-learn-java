5b7050aeeeb943b74ce7fe91b0c4b76f89a7202f
letscodewill <willslima123@gmail.com>
2025-12-24 16:25:28 -0300

    Learned about convertion of types into Numbers and strings

---
+++ src/Fundamentos/ConvertionPrimitiveNumberTypes.java
@@ -0,0 +1,18 @@
+package Fundamentos;
+
+public class ConvertionPrimitiveNumberTypes {
+    public static void main(String[] args) {
+        double a = 1; //automatic convertion
+
+        //float b = 1.0F; //convertion with F
+        float b = (float) 1.0; // (Cast) explicit
+
+        int c = 4;
+        System.out.println(c);
+
+        byte d = (byte) c;
+        System.out.println(d); // (Cast) explicit
+
+
+    }
+}
--- src/Fundamentos/Wrappers.java
+++ src/Fundamentos/Wrappers.java
@@ -1,5 +1,6 @@
 package Fundamentos;
 
+
 public class Wrappers {
     public static void main (String[] args){
 
@@ -8,5 +9,9 @@ public class Wrappers {
         Short s = 1000;
         Integer i = 10000;
         Long l = 100000L;
+
+        Integer newI = Integer.parseInt("1000");
+        System.out.printf("This is a String converted to Integer: %d%n", newI);
+        System.out.println("This is a String: 1000");
     }
 }
