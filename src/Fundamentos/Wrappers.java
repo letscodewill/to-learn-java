5d15f2b0fd4afc99b11440e2e7339cc637d32ce2
letscodewill <willslima123@gmail.com>
2025-12-24 00:09:26 -0300

    Learned about console and Readme.md edited with roadmap of java and somethings

---
+++ src/Fundamentos/Wrappers.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+public class Wrappers {
+    public static void main (String[] args){
+
+        //byte
+        Byte b = 100;
+        Short s = 1000;
+        Integer i = 10000;
+        Long l = 100000L;
+    }
+}
